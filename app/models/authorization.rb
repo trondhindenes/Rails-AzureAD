5a7afe2036568f3a0ce1b9b23963a6913839f093
Trond Hindenes <trond@hindenes.com>
2015-10-21 23:50:27 +0200

    added debugging

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,7 +4,7 @@ class Authorization < ActiveRecord::Base
     
     def self.find_or_create(auth_hash)
       unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
-        logger.debug "auth hash inspect: #{@auth_hash.inspect}"
+        logger.debug "auth hash inspect: #{auth_hash.inspect}"
         user = User.create :name => auth_hash["user_info"]["name"], :email => auth_hash["user_info"]["email"]
         auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       end
