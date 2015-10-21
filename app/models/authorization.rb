04c35df37c503168b7344add99f3c5809d78a1ee
Trond Hindenes <trond@hindenes.com>
2015-10-22 00:35:07 +0200

    updated info

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,6 +4,7 @@ class Authorization < ActiveRecord::Base
     
     def self.find_or_create(auth_hash)
       unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+        #the raw_info object contains all claims coming from Azure AD
         user = User.create :name => auth_hash["raw_info"]["http://schemas.microsoft.com/identity/claims/displayname"], :email => auth_hash["raw_info"]["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"]
         auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
       end
