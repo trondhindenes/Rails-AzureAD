435d9c78f8fd6ed20bc51ce3f2b5c42b1131b79c
Trond Hindenes <trond@hindenes.com>
2015-10-22 00:32:07 +0200

    updated session uid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,9 +16,9 @@ class SessionsController < ApplicationController
       auth = Authorization.find_or_create(auth_hash)
    
       # Create the session
-      session[:user_id] = auth.user.id
+      session[:user_id] = auth.uid
    
-      render :text => "Welcome #{auth.user.name}!"
+      render :text => "Welcome #{auth.uid}!"
     end
   end  
 
